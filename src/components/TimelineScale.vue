d6df754e7830cea634589e5817e0cc0f5d60bc90
Lowell <lxp3901@rit.edu>
2020-05-17 18:09:52 -0400

    add scale components

---
+++ src/components/TimelineScale.vue
@@ -0,0 +1,41 @@
+<template>
+    <g>
+        <g v-for="n in 10" :key="n" :x="position(n)" width="150" :height="height" :fill="color(n)">
+            <TimelineScaleUnit :section-amount="12" :width="150" :height="height" :starting-pos="position(n)" :color="color(n)"/>
+        </g>
+    </g>
+</template>
+
+<script>
+import TimelineScaleUnit from '@/components/TimelineScaleUnit'
+
+    export default {
+        name: 'TimelineScale',
+        components: {
+            TimelineScaleUnit
+        },
+        props: {
+            width: Number,
+            height: Number,
+            subCount: Number
+        },
+        // need to take the width and find a good subwidth to have ten divider rectangles
+        methods: {
+            color: function(n) {
+                if (n % 2 == 0) {
+                    return '#F6F6F6';
+                }
+                else {
+                    return '#FFFFFF';
+                }
+            },
+            position: function(n) {
+                return (n-1) * 150;
+            }
+        } 
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TimelineScaleUnit.vue
@@ -0,0 +1,34 @@
+<template>
+    <g>
+        <rect v-for="n in sectionAmount" :key="n" :x="position(n)" :width="computedWidth" :height="height" :fill="color" class="sub-section"/>
+    </g>
+</template>
+
+<script>
+export default {
+    name: 'TimelineScaleUnit',
+    props: {
+        width: Number,
+        height: Number,
+        startingPos: Number,
+        sectionAmount: Number,
+        color: String
+    },
+    computed: {
+        computedWidth: function() {
+            return this.width / this.sectionAmount;
+        }
+    },
+    methods: {
+        position: function(n) {
+            return this.startingPos + (n - 1) * (this.width / this.sectionAmount);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .sub-section:hover {
+        fill: lightgray;
+    }
+</style>
