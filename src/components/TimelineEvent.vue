d8925fba30eedf3008ab71b7b1687d1cdde2b094
Lowell <lxp3901@rit.edu>
2020-05-28 19:29:51 -0400

    TimelineEvent props for positioning

--- src/components/TimelineEvent.vue
+++ src/components/TimelineEvent.vue
@@ -1,19 +1,20 @@
 <template>
     <g :transform="vertical">
-        <line id="Line_42" data-name="Line 42" :x1="startX" :x2="endX" :y1="y" :y2="y" fill="none" stroke="#ff7272" stroke-width="5"/>
+        <line id="Line_42" data-name="Line 42" :x1="startX + 1" :x2="endX" fill="none" stroke="#ff7272" stroke-width="5"/>
         <g id="Ellipse_5" data-name="Ellipse 5" :transform="startPos" fill="#fff" stroke="#707070">
-            <circle cx="8" cy="8" :r="scale/2" stroke="none"/>
-            <circle cx="8" cy="8" :r="scale/2" fill="none"/>
+            <circle :cx="scale/2" cy="0" :r="scale/2" stroke="none"/>
+            <circle :cx="scale/2" cy="0" :r="scale/2" fill="none"/>
         </g>
         <g id="Ellipse_6" data-name="Ellipse 6" :transform="endPos" fill="#fff" stroke="#707070">
-            <circle cx="8" cy="8" :r="scale/2" stroke="none"/>
-            <circle cx="8" cy="8" :r="scale/2" fill="none"/>
+            <circle :cx="scale/2" cy="0" :r="scale/2" stroke="none"/>
+            <circle :cx="scale/2" cy="0" :r="scale/2" fill="none"/>
         </g>
     </g>
 </template>
 
 <script>
 export default {
+    // transform="translate(160.5 832.5)"
     name: 'TimelineEvent',
     props: {
         name: String,
@@ -31,10 +32,10 @@ export default {
             return `translate(0 ${this.y})`;
         },
         startPos: function () {
-            return `translate(${this.startX} ${this.y})`;
+            return `translate(${this.startX} 0)`;
         },
         endPos:   function () {
-            return `translate(${this.endX} ${this.y})`;
+            return `translate(${this.endX} 0)`;
         }
     }
 }
